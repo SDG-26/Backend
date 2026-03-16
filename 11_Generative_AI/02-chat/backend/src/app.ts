a01a956428597ff21d9d15df275a131e731b431e
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-03-16 12:42:16 +0100

    chat starter

---
+++ 11_Generative_AI/02-chat/backend/src/app.ts
@@ -0,0 +1,31 @@
+import cors from "cors";
+import type { ErrorRequestHandler } from "express";
+import express from "express";
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({ message: "Running" });
+});
+
+app.post("/messages", async (req, res) => {});
+
+app.post("/messages/streaming", async (req, res) => {});
+
+app.post("/images", async (req, res) => {});
+
+app.use("/{*splat}", () => {
+  throw Error("Page not found", { cause: { status: 404 } });
+});
+
+app.use(((err, _req, res, _next) => {
+  console.log(err);
+  res.status(err.cause?.status || 500).json({ message: err.message });
+}) satisfies ErrorRequestHandler);
+
+app.listen(port, () => console.log(`AI Proxy listening on port ${port}`));
